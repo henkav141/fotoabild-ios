962668cef1d73e4d0bd57c4228e6c4c510c6b11d
Johan Wiström <johan.wistrom@reforceinternational.com>
2017-05-27 10:21:39 +0200

    Init commit

---
+++ FotoABild/FotoABild/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FotoABild
+//
+//  Created by Johan Wiström on 2017-05-27.
+//  Copyright © 2017 FotoABild. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
