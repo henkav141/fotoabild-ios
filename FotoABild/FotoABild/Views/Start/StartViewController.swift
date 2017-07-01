53a587a101c2850847db4688b483e4b011959bdc
Johan Wiström <johan.wistrom@reforceinternational.com>
2017-07-01 19:12:36 +0200

    Restructure file system

---
+++ FotoABild/FotoABild/Views/Start/StartViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  FotoABild
+//
+//  Created by Johan Wiström on 2017-05-27.
+//  Copyright © 2017 FotoABild. All rights reserved.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    @IBOutlet weak var orderButton: UIButton!
+    
+    @IBAction func onOrderClick(_ sender: UIButton) {
+        print("Heeelllo!")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+ 
+    
+
+}
+
